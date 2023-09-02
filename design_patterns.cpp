ecbdc807cfb2d861cbe8cc83e09ad6c9200e57c7
Mario Andreev <99554651+m-andreev@users.noreply.github.com>
2023-09-02 20:28:36 +0300

    Initial commit

---
+++ design_patterns.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Wassup!" << endl;
+
+    cout << endl ;
+
+    cout << "Here, I am going to make different branches fo the different design patterns, so check them all out! :) " << endl;
+
+    return 0;
+}
